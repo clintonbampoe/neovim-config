cdd7b5e7c7fb8ea8d099ba7987b628eb9f6155e1
Clinton Bampoe <clintbamp@gmail.com>
2026-06-07 15:44:57 -0500

    chore(plugins): change neo-tree to use ASCII for git status rendering

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,12 +1,28 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  lazy = false,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+      },
+      follow_current_file = { enabled = true },
     },
-    lazy = false, -- neo-tree will lazily load itself
-  }
+    default_component_configs = {
+      git_status = {
+        symbols = {
+          -- Only define the three you want
+          added     = "+",
+          modified  = "*",
+          deleted   = "-",
+        },
+      },
+    },
+  },
 }
