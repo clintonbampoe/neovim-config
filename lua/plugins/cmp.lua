7ee4d467ac2b55694f48d1d13dfd7ed253a16912
Clinton Bampoe <clintbamp@gmail.com>
2026-06-07 15:50:56 -0500

    chore(plugins): set up autocomplete engine

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,55 @@
+return {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp", -- Extracts completions from Roslyn
+        "hrsh7th/cmp-buffer",   -- Extracts words from the current buffer
+        "hrsh7th/cmp-path",     -- Completes file system paths
+        "L3MON4D3/LuaSnip",     -- Required snippet engine
+        "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+        local cmp = require("cmp")
+        local luasnip = require("luasnip")
+
+        cmp.setup({
+
+            -- 1. Enable automatic popup and preselection
+            completion = {
+                autocomplete = { cmp.TriggerEvent.TextChanged },
+                completeopt = "menu,menuone",
+            }, 
+
+            -- 2. Configure preselection behaviour
+            preselect = cmp.PreselectMode.Item,
+
+            snippet = {
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end,
+            },
+
+            mapping = cmp.mapping.preset.insert({
+                ["<C-p>"] = cmp.mapping.select_prev_item(),
+                ["<C-n>"] = cmp.mapping.select_next_item(),
+                
+                -- Use Tab to accept the match if visible, otherwise insert a tab
+                ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+                
+                -- Use Escape to close the menu and stop matching
+                ["<Esc>"] = cmp.mapping.abort(),
+                
+                -- Allow Enter to also confirm
+                ["<CR>"] = cmp.mapping.confirm({ select = false }), 
+            }),
+            
+            sources = cmp.config.sources({
+                { name = "nvim_lsp" },
+                { name = "luasnip" },
+            }, {
+                { name = "buffer" },
+                { name = "path" },
+            })
+        })
+    end,
+}
