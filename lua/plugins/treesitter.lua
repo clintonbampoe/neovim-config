c5562be44ce3412656ed01de3dfe26d94c4c8e45
Clinton Bampoe <clintbamp@gmail.com>
2026-06-07 12:10:32 -0500

    install tree-sitter parser and add Mason registry

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,29 @@
+return {
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  lazy = false,
+  main = 'nvim-treesitter',
+  config = function()
+    require('nvim-treesitter').install({
+      'markdown',
+      'c_sharp',
+      'xml',
+      'javascript',
+      'c',
+      'html',
+      'css',
+      'typescript',
+      'lua',
+      'vim',
+    }, { max_jobs = 8 })
+
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function(args)
+        local lang = vim.treesitter.language.get_lang(vim.bo[args.buf].filetype)
+        if lang then
+          pcall(vim.treesitter.start, args.buf, lang)
+        end
+      end,
+    })
+  end,
+}
