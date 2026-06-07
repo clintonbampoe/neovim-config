ae8f8563e1972d0f96d6368c6a9fd1967bf574cd
Clinton Bampoe <clintbamp@gmail.com>
2026-06-06 21:51:21 -0500

    initial commit

---
+++ init.lua
@@ -0,0 +1,33 @@
+# Config 
+
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+opt.expandtab = true
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.smartindent = true
+opt.termguicolors = true
+opt.background = "dark"
+opt.clipboard = "unnamedplus"
+opt.scrolloff = 8
+opt.sidescrolloff = 8
+opt.wrap = false
+opt.ignorecase = true
+opt.smartcase = true
+opt.updatetime = 250
+
+vim.cmd('syntax on')
+vim.cmd('filetype plugin indent on')
+
+require("config.lazy")
+
+require('neo-tree').setup({
+  source_selector = {
+    winbar = true,
+    status_line = true
+    }
+})
+
+
