90c03c7374c2aa1f0d858aaf5297c04a252d19fb
Clinton Bampoe <clintbamp@gmail.com>
2026-06-07 20:39:48 -0500

    feat(plugins): install trouble.nvim

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,9 @@
+return {
+  "folke/trouble.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {}, -- use default config
+  keys = {
+    { "<leader>tt", "<cmd>Trouble diagnostics toggle<cr>", desc = "Workspace diagnostics" },
+    { "<C-t>", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer diagnostics" },
+  },
+}
