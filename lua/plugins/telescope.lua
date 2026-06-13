f6b8337ea6188b64d1257c49700b2a3910091c66
Clinton Bampoe <clintbamp@gmail.com>
2026-06-13 10:03:46 -0500

    chore(config): set telescope to open in normal mode

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -14,6 +14,7 @@ return {
     defaults = {
       layout_strategy = "horizontal",
       layout_config = { prompt_position = "top" },
+      initial_mode = "normal",
       sorting_strategy = "ascending",
       winblend = 0,
     },
